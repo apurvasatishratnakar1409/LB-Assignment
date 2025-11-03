6cfc65de8231ebf3ed8646fe420618234f6271fd
Apurva Satish Ratnakar <apurvasatishratnakar1409@gmail.com>
2025-11-04 01:44:55 +0530

    program4.c
    This program accepts a single character from the user and converts its case:
    
    If the user enters an uppercase letter (A–Z) → it converts it to lowercase
    
    If the user enters a lowercase letter (a–z) → it converts it to uppercase
    
    🔧 How the Program Works:
    
    The user inputs a character.
    
    The DisplayConvert() function checks:
    
    if(cValue >= 'A' && cValue <= 'Z')
    
    
    → This means the character is uppercase.
    The program converts it to lowercase by adding 32 (ASCII difference between uppercase & lowercase letters).
    
    If the character is lowercase:
    
    else if(cValue >= 'a' && cValue <= 'z')
    
    
    → It converts it to uppercase by subtracting 32.

---
+++ Assignments/Assignments3/program4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void DisplayConvert(char cValue)
+{
+    if(cValue >= 'A' && cValue <= 'Z')
+    {
+        printf(" %d ",cValue + 32);
+    }
+    else if(cValue >= 'a' && cValue <= 'z')
+    {
+        printf(" %d ",cValue - 32);
+    }
+}
+int main()
+{
+    char cValue ='\0';
+    printf("Enter character\n");
+    scanf("%c",&cValue);
+    DisplayConvert(cValue);
+
+    return 0 ;
+    
+}
