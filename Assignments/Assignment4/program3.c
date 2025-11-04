e040d07193e7e7bc952b46dd24cde06ae3074aac
Apurva Satish Ratnakar <apurvasatishratnakar1409@gmail.com>
2025-11-04 08:50:45 +0530

    program3.c
    Program: Display Non-Factors of a Number (C Program)
    
    This C program accepts a number from the user and prints all the non-factors of that number.
    What the Program Does
    Takes an integer input from the user.
    Converts the number to positive if the user enters a negative value.
    Loops from 1 to the given number.
    Prints all numbers that do NOT divide the input number completely.

---
+++ Assignments/Assignment4/program3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void NonFact(int iNo)
+{
+    int i;
+ if(iNo <=0)
+    {
+        iNo = - iNo;
+    }
+    for(i=1;i<=iNo;i++)
+    {
+        if((iNo % i) != 0)
+        {
+            printf("%d\t",i);
+        }
+    }
+}
+
+int main()
+{
+
+    int iValue = 0;
+
+    printf("Enter number:\n");
+    scanf("%d",&iValue);
+
+    NonFact(iValue);
+
+    return 0;
+    
+}
